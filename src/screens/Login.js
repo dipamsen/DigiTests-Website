68788d39f3f99cc68026cd56204ac13aa685099b
Fun Planet <dipamdiptam@gmail.com>
2020-10-14 20:53:15 +0530

    :sparkles: registration

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import firebase from 'firebase';
 import db from '../config'
+import '../styles/login.css'
 
 export default class Login extends Component {
   constructor() {
     super();
     this.state = {
-      email: "",
-      password: ""
+      loginEmail: "",
+      loginPass: "",
+      regName: "",
+      regPass: "",
+      regType: "",
+      regCPass: "",
+      regClass: "",
+      regEmail: "",
     }
   }
   componentDidMount() {
@@ -18,21 +25,54 @@ export default class Login extends Component {
     })
   }
   render() {
-    const { email, password } = this.state
-    const setEmail = e => this.setState({ email: e })
-    const setPassword = e => this.setState({ password: e })
+    const { loginEmail, loginPass, regEmail, regClass, regCPass, regPass, regName, regType } = this.state
+    const setLoginEmail = e => this.setState({ loginEmail: e })
+    const setLoginPassword = e => this.setState({ loginPass: e })
+    const setRegName = e => this.setState({ regName: e })
+    const setRegEmail = e => this.setState({ regEmail: e })
+    const setRegPass = e => this.setState({ regPass: e })
+    const setRegCPass = e => this.setState({ cPass: e })
+    const setRegUserType = e => this.setState({ regType: e })
+
     return (
-      <div>
-        Login to Continue
-        <input type="text" value={email} onChange={x => setEmail(x.target.value)} />
-        <input type="password" value={password} onChange={x => setPassword(x.target.value)} />
-        <button onClick={this.login} type="submit">Login</button>
+      <div className="main">
+        <div className="login">
+          <h2>Login to Continue</h2>
+          <input type="text" placeholder="Email ID" value={loginEmail} onChange={x => setLoginEmail(x.target.value)} />
+          <input type="password" placeholder="Password" value={loginPass} onChange={x => setLoginPassword(x.target.value)} />
+          <button onClick={this.login} type="submit">Login</button>
+        </div>
+        <div className="register">
+          <h2>Register to DigiTests</h2>
+          <input type="text" placeholder="Email ID" value={regEmail} onChange={x => setRegEmail(x.target.value)} />
+          <input type="text" placeholder="Name" value={regName} onChange={x => setRegName(x.target.value)} />
+          <input type="password" placeholder="Password" value={regPass} onChange={x => setRegPass(x.target.value)} />
+          <input type="password" placeholder="Confirm Password" value={regCPass} onChange={x => setRegCPass(x.target.value)} />
+
+          <select onChange={c => setRegUserType(c.target.value)}>
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+          </select>
+          <button onClick={this.register} type="submit">Register</button>
+        </div>
       </div>
     )
   }
   login = () => {
-    const { email, password } = this.state
+    const { loginEmail: email, loginPass: password } = this.state
     firebase.auth().signInWithEmailAndPassword(email, password).catch(err => alert(err.message))
+  }
+  register = () => {
+    const { regName, regEmail, regPass, regType, regCPass, regClass } = this.state
+    if (regPass !== regCPass) return alert("Your Passwords don't match.")
+    firebase.auth().createUserWithEmailAndPassword(regEmail, regPass).then(user => {
+      db.collection("users").add({
+        name: regName,
+        email: regEmail,
+        type: regType,
+        class: regClass,
+      })
+    })
   }
 
 }
