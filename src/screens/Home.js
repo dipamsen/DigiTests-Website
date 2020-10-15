73573405a213f7e564c9e4e1ff5b3e5e07a17dc7
Fun Planet <dipamdiptam@gmail.com>
2020-10-15 14:25:45 +0530

    :sparkles: Styles for login

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import firebase from 'firebase';
-import db from '../config'
+// import db from '../config'
+import '../styles/home.css'
 
 export default class Home extends Component {
   constructor() {
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -11,45 +11,47 @@ export default class Login extends Component {
       loginPass: "",
       regName: "",
       regPass: "",
-      regType: "",
+      regType: "student",
       regCPass: "",
-      regClass: "",
       regEmail: "",
     }
+    this.checkedAuth = false
   }
   componentDidMount() {
     document.title = 'DigiTests | Login';
 
     firebase.auth().onAuthStateChanged(user => {
-      if (user) window.location.replace("/")
+      if (user && !this.checkedAuth) window.location.replace("/")
+      this.checkedAuth = true
     })
   }
   render() {
-    const { loginEmail, loginPass, regEmail, regClass, regCPass, regPass, regName, regType } = this.state
+    const { loginEmail, loginPass, regEmail, regCPass, regPass, regName, regType } = this.state
     const setLoginEmail = e => this.setState({ loginEmail: e })
     const setLoginPassword = e => this.setState({ loginPass: e })
     const setRegName = e => this.setState({ regName: e })
     const setRegEmail = e => this.setState({ regEmail: e })
     const setRegPass = e => this.setState({ regPass: e })
-    const setRegCPass = e => this.setState({ cPass: e })
+    const setRegCPass = e => this.setState({ regCPass: e })
     const setRegUserType = e => this.setState({ regType: e })
 
     return (
       <div className="main">
         <div className="login">
           <h2>Login to Continue</h2>
-          <input type="text" placeholder="Email ID" value={loginEmail} onChange={x => setLoginEmail(x.target.value)} />
+          <input type="email" placeholder="Email ID" value={loginEmail} onChange={x => setLoginEmail(x.target.value)} />
           <input type="password" placeholder="Password" value={loginPass} onChange={x => setLoginPassword(x.target.value)} />
           <button onClick={this.login} type="submit">Login</button>
         </div>
+        <hr />
         <div className="register">
           <h2>Register to DigiTests</h2>
-          <input type="text" placeholder="Email ID" value={regEmail} onChange={x => setRegEmail(x.target.value)} />
           <input type="text" placeholder="Name" value={regName} onChange={x => setRegName(x.target.value)} />
+          <input type="email" placeholder="Email ID" value={regEmail} onChange={x => setRegEmail(x.target.value)} />
           <input type="password" placeholder="Password" value={regPass} onChange={x => setRegPass(x.target.value)} />
           <input type="password" placeholder="Confirm Password" value={regCPass} onChange={x => setRegCPass(x.target.value)} />
 
-          <select onChange={c => setRegUserType(c.target.value)}>
+          <select onChange={c => setRegUserType(c.target.value)} value={regType}>
             <option value="student">Student</option>
             <option value="teacher">Teacher</option>
           </select>
@@ -63,16 +65,18 @@ export default class Login extends Component {
     firebase.auth().signInWithEmailAndPassword(email, password).catch(err => alert(err.message))
   }
   register = () => {
-    const { regName, regEmail, regPass, regType, regCPass, regClass } = this.state
+    const { regName, regEmail, regPass, regType, regCPass } = this.state
     if (regPass !== regCPass) return alert("Your Passwords don't match.")
     firebase.auth().createUserWithEmailAndPassword(regEmail, regPass).then(user => {
       db.collection("users").add({
         name: regName,
         email: regEmail,
         type: regType,
-        class: regClass,
+      }).then(doc => {
+        alert("Account created Successfully.")
+        // window.location.replace("/")
       })
-    })
+    }).catch(err => alert(err.message))
   }
 
 }
